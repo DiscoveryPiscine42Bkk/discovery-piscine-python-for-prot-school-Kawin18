f27677a0228e1b218f3482738911624c2973fcbd
Kawin18 <42944@prot.ac.th>
2025-03-11 11:07:12 +0700

    ex 04

---
+++ cell06/methods_everywhere.py
@@ -0,0 +1,20 @@
+
+def shrink(s):
+    print(s[:8])  
+
+
+def enlarge(s):
+    print(s + 'Z' * (8 - len(s)))  
+
+import sys
+
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for arg in sys.argv[1:]:
+        if len(arg) > 8:
+            shrink(arg)
+        elif len(arg) < 8:
+            enlarge(arg)
+        else:
+            print(arg)
